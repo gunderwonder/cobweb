76242e9990779e5effe8196016e8081d2a99ad06
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-10-27 22:30:31 +0000

    Removing initialization code

--- test/cobweb_core_testcase.class.php
+++ test/cobweb_core_testcase.class.php
@@ -2,16 +2,8 @@
 
 abstract class CobwebCoreTestCase extends UnitTestCase {
 
-	private $is_initialized = false;
 
 	public function initialize($test_application = NULL) {
-		if (!defined('COBWEB_PROJECT_DIRECTORY'))
-			define('COBWEB_PROJECT_DIRECTORY', COBWEB_DIRECTORY . '/test/support/test_application');
 		
-		if (!$this->is_initialized) {
-			Cobweb::initialize();
-			$this->is_initialized = true;
-		}
-			
 	}
 }
