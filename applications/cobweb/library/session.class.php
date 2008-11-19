4b17b0425bf9644907eecc60e389f492a406234e
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-11-19 14:39:35 +0000

    Session cookie is HTTP only by default. Should add a setting for this later

--- applications/cobweb/library/session.class.php
+++ applications/cobweb/library/session.class.php
@@ -1,11 +1,14 @@
 <?php
+/**
+ * @version $Id$
+ */
 
 /**
  * Represents the current session
  * 
  * This class is a simple wrapper around the PHP builtin session functionality.
- * If `INSTALLED_MIDDLEWARE' includes `cobweb.session', a session object is 
- * automatically added to the request object.
+ * If <var>INSTALLED_MIDDLEWARE</var> includes <var>cobweb.session</var>, a session 
+ * object is automatically added to the request object.
  * 
  * @package    Cobweb
  * @subpackage Cobweb Application
@@ -13,7 +16,15 @@
 class Session implements ArrayAccess {
 	
 	public function __construct() {
-		$session_parameters 
+		$session_parameters = session_get_cookie_params();
+		session_set_cookie_params(
+			$session_parameters['lifetime'],
+			$session_parameters['path'],
+			$session_parameters['domain'],
+			$session_parameters['secure'],
+			true
+		);
+		
 		session_start();
 	}
 	
@@ -36,6 +47,12 @@ class Session implements ArrayAccess {
 	public function end() {
 		return session_destroy();
 
+	}
+	
+	public function get($key, $default = NULL) {
+		if (!isset($this[$key]) && !is_null($default))
+			return $default;
+		return $this[$key];
 	}
 	
 	public function __toArray() {
