7a5e853ca00328bc4f705a195b3196fafb5e6662
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-11-19 10:54:22 +0000

    Throw exception when attempting to load applications that are not in `INSTALLED_APPLICATIONS'

--- application/application_manager.class.php
+++ application/application_manager.class.php
@@ -11,15 +11,19 @@ class ApplicationManager {
 		$this->dispatcher = $dispatcher;
 		$this->request = $request;
 		$this->loaded_applications = array();
+		
+		foreach (Cobweb::get('INSTALLED_APPLICATIONS') as $application_name)
+			$this->load($application_name);
 	}
 	
 	public function load($application_name) {
-		if (!in_array($application_name, Cobweb::get('INSTALLED_APPLICATIONS')))
-			throw new CobwebConfigurationException(
-				"'{$application_name}' is not in your 'INSTALLED_APPLICATIONS'.");
 		
 		if (isset($this->loaded_applications[$application_name]))
 			return $this->loaded_applications[$application_name];
+		
+		if (!in_array($application_name, Cobweb::get('INSTALLED_APPLICATIONS')))
+			throw new CobwebConfigurationException(
+				"'{$application_name}' is not in your 'INSTALLED_APPLICATIONS'.");
 		
 		foreach (Cobweb::get('APPLICATIONS_PATH') as $path) {
 			$path = "{$path}/{$application_name}";
