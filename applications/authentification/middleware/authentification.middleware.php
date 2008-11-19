e19be049b03747453a42d9a76e3992b6dcae8ef8
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-11-19 09:38:37 +0000

    Added annotation check and redirect if user is not authentificated

--- applications/authentification/middleware/authentification.middleware.php
+++ applications/authentification/middleware/authentification.middleware.php
@@ -1,22 +1,39 @@
 <?php
 
-class RequiresAuthentification extends Annotation { }
+
 
 class AuthentificationMiddleware extends Middleware {
 	
-	
-	public function processAction(Request $request, Action $action) {
-		$c = $action->reflection();
+	public function initialize() {
 		
-		Cobweb::log($c->getAnnotation('RequiresAuthentification'));
 	}
 	
-	public function processRequest(Request $request) {
-		// if (!isset($request->session))
-		// 	throw new CobwebConfigurationException(
-		// 		"Authentification middleware needs 'cobweb.session' middleware to be installed");
-		// 		
-		// 
+	public function processAction(Request $request, Action $action) {
+		if (isset($request->user))
+			return;
+		
+		if (($user_id = $request->session->get('cobweb-user-id', false))) {
+			
+			$user = Model::table('User')->find($user_id);
+			if ($user) {
+				$request->user = $user;
+				$request->user->setAuthenticated(true);
+				if (!$request->user)
+					$request->user = new User();
+		
+				Cobweb::log('Authenticated user %o', $request->user);
+			} else
+				unset($request->session['cobweb-user-id']);
+				
+		}
+		
+		if (!$request->isAuthenticated() &&
+			$request->path() != Cobweb::get('LOGIN_URL') &&
+			($action->controller()->hasAnnotation('RequiresAuthentification') ||
+		     $action->reflection()->hasAnnotation('RequiresAuthentification'))) {
+			
+			return new HTTPResponseRedirect(Cobweb::get('LOGIN_URL'));
+		}
 	}
 	
 	
