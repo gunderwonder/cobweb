e6caf58f55918ec2cbee7325487c79e3b329580b
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-11-20 15:29:29 +0000

    Added documentation

--- http/response.class.php
+++ http/response.class.php
@@ -1,10 +1,34 @@
 <?php
+/**
+ * @version $Id$
+ */
 
+/**
+ * @author     Øystein Riiser Gundersen <oystein@upstruct.com>
+ * @package    Cobweb
+ * @subpackage HTTP
+ * @version    $Revision$
+ */
 abstract class Response implements ArrayAccess {
 	
 	public $body;
 	
+	/**
+	 * Write the specified content to this response
+	 * @param  string   $contents content to write
+	 * @return Response           this response
+	 */
 	abstract public function write($contents);
+	
+	/**
+	 * Finalizes this response
+	 * @return Response
+	 */
 	abstract public function flush();
+	
+	/**
+	 * Returns the HTTP status code of this response
+	 * @return integer HTTP status code
+	 */
 	abstract public function code();
 }
