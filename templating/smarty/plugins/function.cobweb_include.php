1d9b320c5be009bfda3b7a0720f1b95ee68d1fd8
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2008-11-20 11:54:47 +0000

    Fixed bug in `{cobweb_include}' Smarty function where Smarty would get confused about `compile_dir' when including templates outside the base template's `template_dir'

--- templating/smarty/plugins/function.cobweb_include.php
+++ templating/smarty/plugins/function.cobweb_include.php
@@ -4,9 +4,26 @@ function smarty_function_cobweb_include($parameters, &$smarty) {
 	if (empty($parameters['file']))
 		$smarty->trigger_error('cobweb_include: missing \'file\' argument');
 		
-	$t = new SmartyTemplate();
-	$t->assign($smarty->get_template_vars());
+	// $t = new SmartyTemplate();
+	// 
+	// 
+	// 
+	// $t->assign($smarty->get_template_vars());
+	
+	$old_template_dir = $smarty->template_dir;
+	$old_compire_id = $smarty->compile_id;
+	
+	
+	$template = Template::loadTemplate($parameters['file']);
+	$smarty->template_dir = dirname($template);
+	$smarty->compile_id = dirname($template);
 
-	return $t->fetch(Template::loadTemplate($parameters['file']));
+
+	$result = $smarty->fetch($template);
+	
+	$smarty->template_dir = $old_template_dir;
+	$smarty->compile_id = $old_compire_id;
+	
+	return $result;
 
 }
