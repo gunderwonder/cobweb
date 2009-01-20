1277594447525ddb8ce43216fe6ea8318df36e62
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-01-20 18:31:42 +0000

    Removed some dead code

--- applications/doctrine/library/model.class.php
+++ applications/doctrine/library/model.class.php
@@ -20,27 +20,6 @@ abstract class Model extends Doctrine_Record {
 			
 		parent::__set($key, $value);		
 	}
-	
-	// public function __call($method, $arguments) {
-	// 	if (count($arguments) != 1)
-	// 		return parent::__call($method, $arguments);
-	// 	
-	// 	$matches = array();
-	// 	if (preg_match('/^has(.*)$/', $method, $matches) === false || count($matches) != 2)
-	// 		return parent::__call($method, $arguments);
-	// 
-	// 			
-	// 	$this->hasRelation($relation);
-	// }
-	// 
-	// private function hasRelation_($relation) {
-	// 	if (!isset($this->$relation))
-	// 		return false;
-	// 	foreach ($this->$relation as $r)
-	// 		if ($r->id == $arguments[0])
-	// 			return true;
-	// }
-	
 }
 
 ?>
