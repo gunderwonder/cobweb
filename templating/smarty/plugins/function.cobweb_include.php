c1ec8c77fd5550ecba48fcd7380781e9fb07ce31
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-01-20 18:48:15 +0000

    Minor formatting changes

--- templating/smarty/plugins/function.cobweb_include.php
+++ templating/smarty/plugins/function.cobweb_include.php
@@ -3,21 +3,13 @@
 function smarty_function_cobweb_include($parameters, &$smarty) {
 	if (empty($parameters['file']))
 		$smarty->trigger_error('cobweb_include: missing \'file\' argument');
-		
-	// $t = new SmartyTemplate();
-	// 
-	// 
-	// 
-	// $t->assign($smarty->get_template_vars());
 	
 	$old_template_dir = $smarty->template_dir;
-	$old_compire_id = $smarty->compile_id;
-	
+	$old_compire_id = $smarty->compile_id;	
 	
 	$template = Template::loadTemplate($parameters['file']);
 	$smarty->template_dir = dirname($template);
 	$smarty->compile_id = dirname($template);
-
 
 	$result = $smarty->fetch($template);
 	
