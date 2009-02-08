011b27bfa6d97e8cabf5a555d30e82cbd52b05a6
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-02-08 14:35:04 +0000

    Set `error_reporting' to false when not in `DEBUG' mode, `E_ALL' otherwise

--- templating/smarty/smarty_template.class.php
+++ templating/smarty/smarty_template.class.php
@@ -19,7 +19,8 @@ class SmartyTemplate extends Smarty {
 	 * @return SmartyTemplate
 	 */
 	public function __construct() {
-		$this->error_reporting = E_ALL; 
+		$this->error_reporting = Cobweb::get('DEBUG') ? E_ALL : 0; 
+			
 		$this->debugging = false;
 		$this->plugins_dir = array_merge(
 			$this->plugins_dir,
@@ -37,7 +38,12 @@ class SmartyTemplate extends Smarty {
 		// set $template_dir and $compile_id with respect to template name
 		$this->template_dir = dirname($template);
 		$this->compile_id = dirname($template); 
-		$this->compile_dir = Cobweb::get('COMPILED_TEMPLATES_DIRECTORY');
+		$this->compile_dir = Cobweb::get('COMPILED_TEMPLATES_DIRECTORY', COBWEB_PROJECT_DIRECTORY . '/templates/compiled/');
+		
+		$this->cache_dir = Cobweb::get('TEMPLATE_CACHE_DIRECTORY', COBWEB_PROJECT_DIRECTORY . '/templates/cache/');
+		
+		$this->caching = Cobweb::get('SMARTY_TEMPLATE_CACHING', 0);
+		// $this->compile_check = true;
 
 		$result = parent::fetch(basename($template));
 		
