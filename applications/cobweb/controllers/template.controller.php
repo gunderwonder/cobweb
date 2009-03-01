f1c0bbc95f362f7dac7a4b80af841c0fdcc063c3
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-03-01 13:18:33 +0000

    Added the `TemplateController' class

---
+++ applications/cobweb/controllers/template.controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version $Id$
+ */
+
+/**
+ * @author     Øystein Riiser Gundersen <oystein@upstruct.com>
+ * @package    Cobweb
+ * @subpackage Cobweb Application
+ */
+
+class TemplateController extends Controller {	
+	// use array(cobweb.template.render, template => 'template.tpl') to invoke this controller
+}
