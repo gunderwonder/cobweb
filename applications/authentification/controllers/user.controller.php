7e09539e7fbcc60d2bcb25f119895152f92a6e0b
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-05-20 07:39:35 +0000

    Added fleshed out `login`, `logout`, `logout_and_redirect_to_login` and `redirect_to_login` controller actions.

--- applications/authentification/controllers/user.controller.php
+++ applications/authentification/controllers/user.controller.php
@@ -1,9 +1,36 @@
 <?php
+/* $Id$ */
 
+/**
+ * @author     Øystein Riiser Gundersen
+ * @package    Cobweb
+ * @subpackage Authentification
+ * @version    $Revision$
+ */
 class UserController extends Controller {
 	
-	public function login($template) {
-	
+	public function login($template_name = 'login.tpl') {
+		if ($this->isPOST()) {
+			$username = $this->POST->get('username', '');
+			$password = $this->POST->get('password', '');
+			
+			$user = User::authenticate($username, $password);
+			
+			if ($user) {
+				$this->request->session['cobweb-user-id'] = $user->id;
+				if (!$this->request->isDirect())
+					return new HTTPResponseRedirect($this->request['Referer']);
+				else
+					return new HTTPResponseRedirect(Cobweb::get('LOGIN_REDIRECT_URL'));
+			} else
+				$login_error = true;
+			
+		}
+		
+		return $this->render(
+			'login.tpl', array('login_error' => $login_error),
+			$login_error ? HTTPResponse::UNAUTHORIZED : HTTPResponse::OK
+		);
 	}
 	
 	public function logout($template_name) {
@@ -12,15 +39,16 @@ class UserController extends Controller {
 	}
 	
 	public function logoutAndRedirectToLogin($login_url = NULL) {
-		if (is_null($login_url))
-			$login_url = Cobweb::get('AUTHENTIFICATION_LOGIN_URL');
 		
 		$this->request->user->logout();	
-		return new HTTPResponseRedirect($login_url);
+		return new $this->redirectToLogin($login_url);
 	}
 	
 	public function redirectToLogin($login_url = NULL) {
-		
+		if (is_null($login_url))
+			$login_url = Cobweb::get('LOGIN_URL');
+			
+		return new HTTPResponseRedirect($login_url);
 	}
 	
 }
