ba5e1c4786de0b35ee50e2f3a986d442ef0df9e3
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-05-20 07:56:36 +0000

    Removed some dead code

--- action/include_url_configuration_action.class.php
+++ action/include_url_configuration_action.class.php
@@ -8,9 +8,10 @@ class IncludeURLConfigurationAction implements Action {
 		$this->label = $label;
 		if (count(list($this->application_name, $this->file) = explode('.', $this->label)) != 2)
 			throw new CobwebConfigurationException('Invalid URL configuration label');
+		
 		if (!in_array($this->application_name, Cobweb::get('INSTALLED_APPLICATIONS')))
 			throw new CobwebConfigurationException(
-				"'{$application_name}' is not in your 'INSTALLED_APPLICATIONS'.");
+				"'{$this->application_name}' is not in your 'INSTALLED_APPLICATIONS'.");
 		
 		$this->options = is_null($options) ? array() : $options;
 		$this->rules = NULL;
@@ -54,14 +55,7 @@ class IncludeURLConfigurationAction implements Action {
 		return $this->options;
 	}
 	
-	// public function setResolver(Resolver $resolver) {
-	// 	$this->resolver = $resolver;
-	// }
-	// 
-	// public function resolver() {
-	// 	return $this->resolver;
-	// }
-	
+
 	public function name() {
 		// XXX: should throw exception here
 		return '';
