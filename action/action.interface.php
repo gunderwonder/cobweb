4f7170e2c260f01ed7224d64e8c4ea7e29b548cc
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-07-06 11:53:44 +0000

    Documentation++

--- action/action.interface.php
+++ action/action.interface.php
@@ -1,8 +1,39 @@
 <?php
+/**
+ * @version $Id$
+ * @licence http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @copyright Upstruct Berlin Oslo
+ */
 
+/**
+ * @author Øystein Riiser Gundersen <oystein@upstruct.com>
+ * @package Cobweb
+ * @subpackage Action
+ */
 interface Action  {
+	
+	/**
+	 * Invoke this action, returning a {@link Response} object
+	 * @param array $arguments the arguments to invoke the actio with
+	 * @return Response
+	 */
 	public function invoke(array $arguments = NULL);
 	
+	/**
+	 * Returns true if this action is annotated with the specified annotation,
+	 * false otherwise
+	 * 
+	 * @param string $annotation the name of the annotation
+	 * @return bool if this action is annotated with the specified annotation
+	 */
 	public function hasAnnotation($annotation);
+	
+	/**
+	 * Returns the value of the specified annotation. Throws {@link CobwebErrorException}
+	 * if this action is not annotated with the annotation.
+	 * 
+	 * @param string $annotation the name of the annotation to retrieve
+	 * @return mixed the value of the annotation
+	 */
 	public function annotation($annotation);
 }
