33a6235bfe5ae5ab0602d6f167b853b3386d6c8b
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-07-08 13:56:13 +0000

    Sigh. Added `require_once` for the bootstrap file

--- manager/project_template/www/index.php
+++ manager/project_template/www/index.php
@@ -4,4 +4,6 @@ define('COBWEB_DIRECTORY', '%cobweb_directory%');
 define('COBWEB_PROJECT_DIRECTORY', "%project_directory%");
 define('COBWEB_WWW_ROOT', COBWEB_PROJECT_DIRECTORY . '/www');
 
+require_once COBWEB_DIRECTORY . '/core/cobweb_bootstrap.inc.php';
+
 Cobweb::start();
