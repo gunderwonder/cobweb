cca3fd74d33603088ce2c20b3ac1dd3f1ef6946d
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-08-03 08:04:22 +0000

    Minor refactoring of the `cobweb_reverse_url_map` template plugin

--- templating/smarty/plugins/function.cobweb_reverse_url_map.php
+++ templating/smarty/plugins/function.cobweb_reverse_url_map.php
@@ -16,12 +16,8 @@ function smarty_function_cobweb_reverse_url_map($parameters, &$smarty) {
 	$map = array();
 	$reverse = Cobweb::get('__RESOLVER__')->reverseMap();
 	foreach ($reverse as $a => $p)
-		$map[$a] = preg_to_jsregexp($p);
+		$map[$a] = preg_replace('/(\()(\?P?<\w+>)(.*?\))/', '$1$3', $p);
 
 	return JSON::encode($map);
 	
-}
-
-function preg_to_jsregexp($preg) {
-	return preg_replace('/(\()(\?P?<\w+>)(.*?\))/', '$1$3', $preg);
 }
