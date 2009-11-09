6e08211900aa109eeba8c25065cb314dc2268a7f
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-11-09 12:01:12 +0000

    Comments++

--- utilities/mutable_array.class.php
+++ utilities/mutable_array.class.php
@@ -5,6 +5,10 @@
  * @copyright Upstruct Berlin Oslo
  */
 
+/**
+ * @package Cobweb
+ * @subpackage Utilities
+ */
 class MutableArray implements ArrayAccess, IteratorAggregate, Countable {
 	
 	protected $array;
