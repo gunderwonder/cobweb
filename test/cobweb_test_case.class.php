7deb8ab9a14007e1c43906c9ae287c1a07483de1
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-11-13 15:55:52 +0000

    Added simple `dump()` method for delayed data output during test run.

--- test/cobweb_test_case.class.php
+++ test/cobweb_test_case.class.php
@@ -2,4 +2,17 @@
 
 class CobwebTestCase extends PHPUnit_Framework_TestCase {
 	
+	private $__output = '';
+	
+	public function dump() {
+		$trace = debug_backtrace();
+		$function = $trace[1]['class'] . '::' . $trace[1]['function'] . '()' . "\n";
+		$arguments = func_get_args();
+		$output = implode("\n", array_map('stringify', $arguments));
+		$this->__output .= "\n" . $function . $output . "\n";
+	}
+	
+	public function __destruct() {
+		echo $this->__output;
+	}
 }
