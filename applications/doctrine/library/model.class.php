7d236182742dbf6ae1eafda0e83802dc4e533c42
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-11-16 14:22:24 +0000

    Objects which implements `__toSQL()` gets special treatment (HACK!)

--- applications/doctrine/library/model.class.php
+++ applications/doctrine/library/model.class.php
@@ -20,15 +20,12 @@ abstract class Model extends Doctrine_Record {
 	public static function query($model_name = NULL, $query_alias = '') {
 		if (is_null($model_name))
 			return Doctrine_Query::create();
-		
 		return Doctrine::getTable($model_name)->createQuery($query_alias);
 	}
 	
 	public function __set($key, $value) {
-		if (is_object($value) && method_exists($value, '__toSQL')) 
-			$value = $value->__toSQL();
-			
-		parent::__set($key, $value);		
+		if (method_exists($value, '__toSQL')) $value = $value->__toSQL();
+		parent::__set($key, $value);
 	}
 }
 
