848604242c653c6f004c82592ba39575391079bc
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2009-12-06 12:43:25 +0000

    Formatting changes, and added default `LOGIN_URL` and `LOGIN_REDIRECT_URL` settings.

--- settings/settings.conf.php
+++ settings/settings.conf.php
@@ -14,31 +14,27 @@ $settings = array(
 	'TIMEZONE' => 'Europe/Oslo',
 	'TEMPLATE_PROCESSORS' => array('Cobweb::processTemplate'),
 	'APPEND_SLASH_ON_404' => true,
-	'URL_CONFIGURATION' => array('^$' => 'cobweb.cobweb.start')
+	'URL_CONFIGURATION' => array('^$' => 'cobweb.cobweb.start'),
+	'LOGIN_URL' => '/accounts/login',
+	'LOGIN_REDIRECT_URL' => '/accounts/profile'
 );
 
 if (defined('COBWEB_PROJECT_DIRECTORY'))
-	$settings = array_merge($settings,
-		array(
-			'APPLICATIONS_PATH' => array(
-				COBWEB_DIRECTORY . '/applications',
-				COBWEB_PROJECT_DIRECTORY . '/applications'
-			),
-			'COMPILED_TEMPLATES_DIRECTORY' => COBWEB_PROJECT_DIRECTORY . '/templates/compiled',
-			'TEMPLATE_DIRECTORIES' => array(
-				COBWEB_PROJECT_DIRECTORY . '/templates',
-				COBWEB_DIRECTORY . '/applications/cobweb/templates'
-			)
+	$settings = array_merge($settings, array(
+		'APPLICATIONS_PATH' => array(
+			COBWEB_DIRECTORY . '/applications',
+			COBWEB_PROJECT_DIRECTORY . '/applications'
+		),
+		'COMPILED_TEMPLATES_DIRECTORY' => COBWEB_PROJECT_DIRECTORY . '/templates/compiled',
+		'TEMPLATE_DIRECTORIES' => array(
+			COBWEB_PROJECT_DIRECTORY . '/templates',
+			COBWEB_DIRECTORY . '/applications/cobweb/templates'
+		)
 	));
 	
 else
-	$settings = array_merge($settings,
-		array(
-			'APPLICATIONS_PATH' => array(
-				COBWEB_DIRECTORY . '/applications',
-			),
-			'TEMPLATE_DIRECTORIES' => array(
-				COBWEB_DIRECTORY . '/applications/cobweb/templates'
-			)
+	$settings = array_merge($settings, array(
+			'APPLICATIONS_PATH' => array(COBWEB_DIRECTORY . '/applications'),
+			'TEMPLATE_DIRECTORIES' => array(COBWEB_DIRECTORY . '/applications/cobweb/templates')
 	));
 return $settings;
