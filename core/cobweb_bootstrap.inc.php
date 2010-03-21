87756289e8e9f9e6c26e4348a627fefc95230b4e
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2010-03-21 01:17:49 +0000

    `require_once` => `require`

--- core/cobweb_bootstrap.inc.php
+++ core/cobweb_bootstrap.inc.php
@@ -7,20 +7,20 @@
 
 error_reporting(E_ALL);
 
-require_once COBWEB_DIRECTORY . '/core/exceptions.inc.php';
-require_once COBWEB_DIRECTORY . '/core/cobweb_loader.class.php';
-require_once COBWEB_DIRECTORY . '/core/cobweb_declaration.interface.php';
-
-require_once COBWEB_DIRECTORY . '/core/cobweb.class.php';
+require COBWEB_DIRECTORY . '/core/exceptions.inc.php';
+require COBWEB_DIRECTORY . '/core/cobweb_loader.class.php';
+require COBWEB_DIRECTORY . '/core/cobweb_declaration.interface.php';
+       
+require COBWEB_DIRECTORY . '/core/cobweb.class.php';
 
 spl_autoload_register(array('CobwebLoader', 'load'));
 set_error_handler(array('Cobweb', 'handleError'));
 
-require_once COBWEB_DIRECTORY . '/vendor/utf8/utf8.php';
-require_once COBWEB_DIRECTORY . '/vendor/utf8/str_ireplace.php';
-
-require_once COBWEB_DIRECTORY . '/utilities/miscellaneous.inc.php';
-require_once COBWEB_DIRECTORY . '/utilities/string.inc.php';
-require_once COBWEB_DIRECTORY . '/utilities/string_inflection.inc.php';
-
-require_once COBWEB_DIRECTORY . '/utilities/array.inc.php';
+require COBWEB_DIRECTORY . '/vendor/utf8/utf8.php';
+require COBWEB_DIRECTORY . '/vendor/utf8/str_ireplace.php';
+       
+require COBWEB_DIRECTORY . '/utilities/miscellaneous.inc.php';
+require COBWEB_DIRECTORY . '/utilities/string.inc.php';
+require COBWEB_DIRECTORY . '/utilities/string_inflection.inc.php';
+       
+require COBWEB_DIRECTORY . '/utilities/array.inc.php';
