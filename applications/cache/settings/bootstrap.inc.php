881a381443ecfd441c1b8a2c67b8a51afe5b50cb
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2010-03-21 01:24:08 +0000

    Added model autoloading for `CobwebCache`.

--- applications/cache/settings/bootstrap.inc.php
+++ applications/cache/settings/bootstrap.inc.php
@@ -3,6 +3,7 @@
 CobwebLoader::autoload(CACHE_APPLICATION_DIRECTORY, array(
 	'DatabaseCacheEngine' => '/engine/database_cache_engine.class.php',
 	'MemcachedCacheEngine' => '/engine/memcached_cache_engine.class.php',
+	'CobwebCache' => '/models/cobweb_cache.model.php'
 ));
 
 CacheStore::initialize();
